14e76ebca1a7dda360bdde1bc1ecfef3509623a7
Valentine Kibe <valentinekibe@gmail.com>
2024-08-25 23:22:09 +0300

    user input

--- main.js
+++ main.js
@@ -3,5 +3,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
   form.addEventListener("submit", function (event) {
     event.preventDefault();
+
+    // Get user input values
+    const income = parseFloat(document.getElementById("income").value);
+    const expenses = parseFloat(document.getElementById("expenses").value);
+    const savingsGoal = parseFloat(
+      document.getElementById("savings-goal").value
+    );
+    const savingsPercentage = parseFloat(
+      document.getElementById("savings-percentage").value
+    );
   });
 });
